5feb610581c11668947317e2b3e50f6f1b51189f
Sky-Aurora <3326575556@qq.com>
2025-05-31 15:42:46 +0800

    first commit

--- target/classes/static/js/main.js
+++ target/classes/static/js/main.js
@@ -14,15 +14,16 @@ let _move = () => {
         document.querySelector('#slide').appendChild(lists[0]);
     }else{
         document.querySelector('#slide').prepend(lists[lists.length - 1]);
-    }
+    }  
 }
 
-let timer = setInterval(_move,1500);
+let timer = setInterval(_move,2500);
 document.querySelector('.container').addEventListener('mouseover', () =>{
     clearInterval(timer);
 });
 document.querySelector('.container').addEventListener('mouseout', () =>{
-    timer = setInterval(_move,1500);
+    clearInterval(timer); // 先清除旧定时器
+    timer = setInterval(_move,2500);
 });
 
 document.querySelectorAll('.s_button')[1].onclick = () =>{
